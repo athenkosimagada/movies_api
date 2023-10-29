5b196de60f2a0523b4d709b4757c379c1901110b
Athenkosi Magada <magadaathenkosi@gmail.com>
2023-10-29 10:58:19 +0200

    Deleted code no used

--- src/pages/MoviesShows.tsx
+++ src/pages/MoviesShows.tsx
@@ -1,5 +1,3 @@
-import React from 'react'
-
 function MoviesShows() {
   return (
     <div>MoviesShows</div>
--- src/pages/NoPage.tsx
+++ src/pages/NoPage.tsx
@@ -1,5 +1,3 @@
-import React from 'react'
-
 function NoPage() {
   return (
     <div>NoPage</div>
