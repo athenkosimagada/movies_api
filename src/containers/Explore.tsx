8cd7f53038d397e392032c74465c18d8edceee25
Athenkosi Magada <magadaathenkosi@gmail.com>
2023-12-04 22:08:02 +0200

    Update device responsiveness

--- src/containers/Explore.tsx
+++ src/containers/Explore.tsx
@@ -7,11 +7,40 @@ import "swiper/css/grid";
 import "swiper/css/pagination";
 import "swiper/css/navigation";
 
-import { Grid, Pagination, Navigation} from "swiper/modules";
+import { Grid, Pagination, Navigation } from "swiper/modules";
 
 import Genre from "../components/Genre";
+import { useEffect, useState } from "react";
 
 function Explore() {
+  const [IsMobile, setIsMobile] = useState<Boolean>(window.innerWidth < 768);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth < 768);
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  const breaks = {
+    0: {
+      slidesPerView: 2,
+    },
+    480: {
+      slidesPerView: 3,
+    },
+    768: {
+      slidesPerView: 4,
+    },
+    1024: {
+      slidesPerView: 5,
+    },
+  };
   return (
     <div className="container explore">
       <div className="explore__container">
@@ -22,49 +51,32 @@ function Explore() {
             make you think, or a documentary to learn something new
           </p>
         </div>
-        <div className="explore__controler">
-          <div className="swiper-button__prev slider__arrow">
-            <HiArrowNarrowLeft />
+        <div className={`explore__controler ${IsMobile ? 'mobile__pagination' : ''}`}>
+            <div className="swiper-button__prev slider__arrow">
+              <HiArrowNarrowLeft />
+            </div>
+            <div className='swiper__pagination'></div>
+            <div className="swiper-button__next slider__arrow">
+              <HiArrowNarrowRight />
+            </div>
           </div>
-          <div className="swiper__pagination"></div>
-          <div className="swiper-button__next slider__arrow">
-            <HiArrowNarrowRight />
-          </div>
-        </div>
       </div>
       <div className="explore-type">
         <Swiper
           navigation={{
-            nextEl: '.swiper-button__next',
-            prevEl: '.swiper-button__prev'
+            nextEl: ".swiper-button__next",
+            prevEl: ".swiper-button__prev",
           }}
           grid={{
             rows: 1,
           }}
           spaceBetween={10}
           pagination={{
-            el: '.swiper__pagination',
+            el: ".swiper__pagination",
             clickable: true,
           }}
           modules={[Grid, Pagination, Navigation]}
-          breakpoints={{
-            // when window width is >= 320px
-            0: {
-              slidesPerView: 1,
-            },
-            // when window width is >= 480px
-            480: {
-              slidesPerView: 2,
-            },
-            // when window width is >= 640px
-            768: {
-              slidesPerView: 3,
-            },
-            // when window width is >= 768px
-            1024: {
-              slidesPerView: 4,
-            },
-          }}
+          breakpoints={breaks}
           loop={true}
           className="genre-swiper"
         >
